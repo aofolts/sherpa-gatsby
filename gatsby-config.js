f158e47a5a46bcbe0a8b320b22131d633c773bfa
Andrew Folts <andrew@sherpadesign.co>
2018-09-21 21:20:02 -0400

    Revised Google Analytics Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,9 +67,13 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-106772115-3",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
         anonymize: true,
+        // Setting this parameter is also optional
         respectDNT: true
-      }
-    }
+      },
+    },
   ]
 }
