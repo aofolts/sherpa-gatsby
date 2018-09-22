a818dcee0bc17fbc5589ed702ccbe3f264f02d81
Andrew Folts <andrew@sherpadesign.co>
2018-09-21 20:58:41 -0400

    Removed manual Google Analytics

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,18 +4,6 @@ import Footer from './footer'
 import {Helmet} from 'react-helmet'
 import favicon from '../images/favicon.png'
 
-const gaScript = `
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106772115-3"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-106772115-3');
-  </script>
-`
-
 const Layout = ({
   children
 }) => {
@@ -24,7 +12,6 @@ const Layout = ({
       <Helmet>
         <meta charSet="utf-8" />
         <link rel='shortcut icon' type='image/png' href={favicon}/>
-        {gaScript}
       </Helmet>
       <Header/>
       {children}
