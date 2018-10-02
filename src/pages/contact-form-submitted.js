6fdb03d93c74205e988b555c1b698369517d4960
aofolts <aofolts@gmail.com>
2018-10-02 13:39:46 -0500

    Added contact form submission page

---
+++ src/pages/contact-form-submitted.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import css from '../less/notfound.module.less'
+import gif from '../images/success.gif'
+import {Link} from 'gatsby'
+
+const Success = () => (
+  <div className={css.page}>
+    <div className={css.container}>
+      <div className={css.media}>
+        <img src={gif} alt='Retro computer kid thumbs up' className='mediaBackground'/>
+      </div>
+      <div className={css.content}>
+        <h1 className={css.headline}>Message sent!</h1>
+        <p>Ready to dive back in?</p>
+        <Link to='/' className='primaryButton'>
+          Return Home
+        </Link>
+      </div>
+    </div>
+  </div>
+)
+
+export default Success
